62b6dff8a312d0dd3c1c8646fd0915a2c5c37195
Benjamin Piro <brp8396@rit.edu>
2021-12-31 17:42:13 -0500

    Created logo that will need to be fixed later

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
+// Assets
+import logo from '../../assets/logo.png';
+import logo_invert from '../../assets/logo_invert.png';
+
 function Title(props) {
     const [isTitleHover, setIsTitleHover] = useState(false);
     const titleName = props.title.split(' ');
+    const titleLogo = (<img className="icon" src={isTitleHover ? logo_invert : logo}></img>);
     const fname = (<span style={isTitleHover ? {} : {color: '#ef5951'}}>{titleName[0]}</span>);
     const lname = (<span style={isTitleHover ? {color: '#ef5951'} : {}}>{titleName[1]}</span>);
     return (
         <div className="title text"
                 onMouseEnter={() => setIsTitleHover(true)}
                 onMouseLeave={() => setIsTitleHover(false)}>
-                <Link to="/"><div><span >{fname} {lname}</span></div></Link>
+                <Link to="/"><div>{titleLogo} <span>{fname} {lname}</span></div></Link>
         </div>
     );
 }
@@ -22,17 +27,18 @@ function Element(props) {
 
 function Navbar() {
     return (
-        <div className="nav-container noselect">
-            <Title title="Benjamin Piro" />
-            <div className="nav-elements">
-                <Link to="/"><Element name="About"/></Link>
-                <Link to="/projects"><Element name="Projects"/></Link>
-                <Link to="/experience"><Element name="Experience"/></Link>
-                <Link to="/extra"><Element name="Extra"/></Link>
-                <a href='https://github.com/bean217'><Element name="GitHub"/></a>
+        <nav>
+            <div className="nav-container noselect">
+                <Title title="Benjamin Piro" />
+                <div className="nav-elements">
+                    <Link to="/"><Element name="About"/></Link>
+                    <Link to="/projects"><Element name="Projects"/></Link>
+                    <Link to="/experience"><Element name="Experience"/></Link>
+                    <Link to="/extra"><Element name="Extra"/></Link>
+                    <a href='https://github.com/bean217'><Element name="GitHub"/></a>
+                </div>
             </div>
-            
-        </div>
+        </nav>
     );
 }
 
