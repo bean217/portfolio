9a0e4afedae4e88a8b8e5a88ac64fd1d90585cea
Benjamin Piro <brp8396@rit.edu>
2022-01-22 20:29:05 -0500

    Removed /routing, so now all routing now happens from App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import './App.css';
-//import Navbar from './components/Navbar/Navbar';
-import Main from './routing/Main';
 import React from 'react'
 
+// Routing
+import About from './pages/About/About';
+import Projects from './pages/Projects/Projects';
+import Experience from './pages/Experience/Experience';
+import Extra from './pages/Extra/Extra';
+import { Routes, Route} from 'react-router-dom';
+
+// Hooks
 import useNavbar from './components/Navbar/Navbar';
 
 export const NavDimsContext = React.createContext();
 
 function App() {
+    console.log("App render")
     const { navDims, navRender } = useNavbar();
-
     return (
         <div className="App">
-            {navRender}
             <NavDimsContext.Provider value={navDims}>
-                <Main />
+                {navRender}
+                <Routes>
+                    <Route path='/' element={<About />}></Route>
+                    <Route path='/projects' element={<Projects />}></Route>
+                    <Route path='/experience' element={<Experience />}></Route>
+                    <Route path='/extra' element={<Extra />}></Route>
+                </Routes>
             </NavDimsContext.Provider>
+
         </div>
     );
 }
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,10 +2,13 @@ import React, { useContext } from 'react';
 import { NavDimsContext } from '../../App';
 import './About.css';
 
-const About = (props) => {
-    const navDims = useContext(NavDimsContext);
-    const scrollPosition = props.scrollPosition;
+// Hooks
+import useScrollPosition from '../../hooks/useScrollPosition';
 
+const About = () => {
+    console.log("About render")
+    const navDims = useContext(NavDimsContext);
+    const scrollPosition = useScrollPosition();
     return (
         <div>
             <div 
