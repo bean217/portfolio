afc6a6c628f1639937ed8d5f63cdcd8bb36eedef
Benjamin Piro <brp8396@rit.edu>
2022-01-23 22:06:04 -0500

    Added bar on home page showing brief list of education/experience

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,32 +3,50 @@ import './About.css';
 
 // Assets
 import benpic from '../../assets/benpiro.png';
+import ritlogo from '../../assets/RIT_seal.png';
+import ritcsh from '../../assets/rit_csh.png';
+import carestream from '../../assets/carestream.png';
 
 // Hooks
 import useScrollPosition from '../../hooks/useScrollPosition';
 import useWindowSize from '../../hooks/useWindowSize';
 
-const About = () => {
-    console.log("About render")
+const Landing = () => {
     const scrollPosition = useScrollPosition();
     const windowSize = useWindowSize();
     const navHeight = document && document.getElementById("nav") && document.getElementById("nav").offsetHeight;
     return (
+        <div 
+            className="landing-container" 
+            style={{
+                height: `${windowSize.height - navHeight}px`,
+                opacity: `${1 - (scrollPosition / windowSize.height)}`,
+                backgroundColor: "red"}}>
+            <div className="landing">
+                <div className="landingphoto landingitem"><img src={benpic} alt=""></img></div>
+                <div className="landingname landingitem">Benjamin Piro</div>
+                <div className="landingtext landingitem">Software Engineering Intern at Carestream Health, Inc. - Undergraduate Student in Computer Science</div>
+            </div>
+        </div>
+    );
+};
+
+const LandingExperience = () => {
+    return (
+        <div className="landingexperience">
+            <div className="rit"><img src={ritlogo} alt=""></img></div>
+            <div className="csh"><img src={ritcsh} alt=""></img></div>
+            <div className="carestream"><img src={carestream} alt=""></img></div>
+        </div>
+    );
+};
+
+const About = () => {
+    console.log("About render");
+    return (
         <div>
-            <div 
-                className="landing" 
-                style={{
-                    height: `${windowSize.height - navHeight}px`,
-                    opacity: `${1 - (scrollPosition / windowSize.height)}`,
-                    backgroundColor: "red"}}>
-                <img className="landingphoto" src={benpic} alt=""></img>        
-            </div>
-            <div  
-                style={{
-                    height: "100vh"}}>
-                ScrollPosition: {scrollPosition}<br></br>
-                NavHeight: {navHeight}
-            </div>
+            <Landing/>
+            <LandingExperience/>
         </div>
     );
 };
