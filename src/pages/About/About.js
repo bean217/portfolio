081ec94a25cb673965352aad40b864af53682766
Benjamin Piro <brp8396@rit.edu>
2022-01-25 22:40:25 -0500

    Started adding styling to description section

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -47,49 +47,58 @@ const LandingExperience = () => {
 const Description = () => {
     return (
         <div className="description-container">
-            <div class="description">
-                <div className="description-photo description-item"><img src={benpic} alt=""></img></div>
-                <div><span>Benjamin Piro</span></div>
-                <div className="description-icons-wrapper">
-                    <div className="description-icon">
-                        <a 
-                            href="https://www.facebook.com/saikofilms" 
-                            rel="noreferrer" 
-                            className="fa fa-facebook" 
-                            target="_blank" >
-                        </a>
-                    </div>
-                    <div className="description-icon">
-                        <a 
-                            href="https://www.linkedin.com/in/benjamin-piro-38427a195/"
-                            rel="noreferrer" 
-                            className="fa fa-linkedin" 
-                            target="_blank" >
-                        </a>
-                    </div>
-                    <div className="description-icon">
-                        <a 
-                            href="https://github.com/bean217" 
-                            rel="noreferrer" 
-                            className="fa fa-github" 
-                            target="_blank" >
-                        </a>
+            <div className="description">
+                <div className="description-assets">
+                    <div className="description-photo description-item"><img src={benpic} alt=""></img></div>
+                    <div className="description-icons-wrapper">
+                        <div className="description-icon">
+                            <a 
+                                href="https://www.facebook.com/saikofilms" 
+                                rel="noreferrer" 
+                                className="fa fa-facebook" 
+                                target="_blank" >
+                            </a>
+                        </div>
+                        <div className="description-icon">
+                            <a 
+                                href="https://www.linkedin.com/in/benjamin-piro-38427a195/"
+                                rel="noreferrer" 
+                                className="fa fa-linkedin" 
+                                target="_blank" >
+                            </a>
+                        </div>
+                        <div className="description-icon">
+                            <a 
+                                href="https://github.com/bean217" 
+                                rel="noreferrer" 
+                                className="fa fa-github" 
+                                target="_blank" >
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <div><span>RIT '24, Computer Science Student, Software Engineer, and Musician</span></div>
-                <p>I'm a current undergraduate student at the Rochester Institute of Technology and 
-                    I have a deep passion for both computer science and music. My interests included
-                    software development, computational mathematics, artificial intelligent, and machine
-                    learning. In my leisure, I enjoy performing as a tubist in the RIT Philharmonic
-                    Orchestra and participating as a member and section leader of The Cadets Drum
-                    &amp; Bugle Corps tuba section.</p>
-                <div><span>Rochester Institute of Technology (RIT)</span></div>
-                <ul>
-                    <li>Bachelor of Science in Computer Science</li>
-                    <li>Immersion in German Language &amp; Culture</li>
-                    <li>Alumnus of Computer Science House</li>
-                    <li>Outstanding Undergraduate Scholar Award Recipient (awarded to the top 1% undergraduate students at RIT)</li>
-                </ul>
+                <div className="description-text">
+                    <div className="name"><span>Benjamin Piro</span></div>
+                    <div className="education"><span>RIT '24, Computer Science Student, Software Engineer, and Musician</span></div>
+                    <span className="text">I am a current undergraduate student at the Rochester Institute of Technology and 
+                        I have a deep passion for both computer science and music. My interests included
+                        software development, computational mathematics, artificial intelligent, and machine
+                        learning. In my leisure, I enjoy performing as a tubist in the RIT Philharmonic
+                        Orchestra and participating as a member and section leader of The Cadets Drum
+                        &amp; Bugle Corps tuba section.</span>
+                    <br/>
+                    <span className="text">I am currently working as a software engineer intern at Carestream Health, Inc. as a member
+                        of their ImageView team. This team is responsible for developing and maintaining the user
+                        interfaces used on Carestream's suite of X-ray machines.
+                    </span>
+                    <div className="education"><span>Rochester Institute of Technology (RIT)</span></div>
+                    <ul className="education-list">
+                        <li><b>Bachelor of Science in Computer Science</b></li>
+                        <li><b>Immersion in German Language &amp; Culture</b></li>
+                        <li>Alumnus of Computer Science House</li>
+                        <li>Outstanding Undergraduate Scholar Award Recipient (awarded to the top 1% undergraduate students at RIT)</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
