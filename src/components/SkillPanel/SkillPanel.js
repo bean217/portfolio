6881360f492b204893faeeac8b5accf6c0cc4307
Benjamin Piro <brp8396@rit.edu>
2022-01-31 01:17:31 -0500

    Added Skills section to about page

---
+++ src/components/SkillPanel/SkillPanel.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SkillPanel.css";
+
+function SkillPanel(props) {
+    const skillData = props.skillData;
+    return (
+        <div className="skill-panel">
+            <div className="skill-header">
+                <div className="skill-icon"></div>
+                <div className="skill-title">{skillData.title}</div>
+            </div>
+            <div className="skill-description">{skillData.description}</div>
+        </div>
+    );
+}
+
+export default SkillPanel;
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './About.css';
 
+// Components
+import SkillPanel from "../../components/SkillPanel/SkillPanel";
+
 // Assets
 import benpic from '../../assets/benpiro.png';
 import ritlogo from '../../assets/RIT_seal.png';
@@ -11,6 +14,75 @@ import crossmen from '../../assets/crossmen-productions-logo.jpg';
 // Hooks
 import useScrollPosition from '../../hooks/useScrollPosition';
 import useWindowSize from '../../hooks/useWindowSize';
+
+const SkillsData = [
+    {
+        icon: {},
+        title: "C Programming Language",
+        description: "The majority of my experience stems from a \
+        course I took called Mechanics of Programming. I have an \
+        ongoing side project in C using the NCurses library to \
+        create a terminal ASCII UI for interacting with a Conway’s \
+        Game of Life simulator."
+    },
+    {
+        icon: {},
+        title: "Java Programming Language",
+        description: "Java is a language often used in undergraduate \
+        programming courses at RIT. Most of my experience in Java is \
+        a result of the time I spent in my second semester computer \
+        science course, computer science theory course, and algorithms \
+        course."
+    },
+    {
+        icon: {},
+        title: "C#/.NET",
+        description: "Most of my video game oriented personal projects \
+        have been written in C#/.NET in collaboration with the MonoGame \
+        framework. For more information on related projects, please visit \
+        my projects tab."
+    },
+    {
+        icon: {},
+        title: "Python Programming Language",
+        description: "I have experience using Python to write small tools and \
+        participate in coding challenges, such as the annual Advent of Code holiday \
+        coding challenge. In recent months, I have started to use Python for learning \
+        and writing applications that involve AI and machine learning."
+    },
+    {
+        icon: {},
+        title: "ReactJS",
+        description: "I have some experience using React to develop web \
+        applications as a means of interfacing with tools that I have built, \
+        as well as this website! I am looking to become more familiar with \
+        React through future personal projects related to AI and machine learning."
+    },
+    {
+        icon: {},
+        title: "JavaScript/HTML5/CSS",
+        description: "I have some experience using JavaScript, HTML and CSS to \
+        develop a few web applications, as well as this website. During my first \
+        co-op with Carestream Health, Inc. I spent the majority of my time writing \
+        JavaScript using AngularJS."
+    },
+    {
+        icon: {},
+        title: "Agile Development",
+        description: "I first got some experience with Agile development in my \
+        introduction to software engineering course where I built a checkers web \
+        application along with a team of four other developers. I also have experience \
+        using Agile development in my first co-op with Carestream Health, Inc."
+    },
+    {
+        icon: {},
+        title: "Git/GitHub/Azure DevOps",
+        description: "Throughout my college life and professional life I have used git \
+        through the command line in conjunction with GitHub and Azure DevOps. While I have \
+        predominantly used GitHub for my personal work, I have used Azure DevOps in my work \
+        at Carestream Health, Inc."
+    }
+];
 
 const Landing = () => {
     const scrollPosition = useScrollPosition();
@@ -109,7 +181,13 @@ const Description = () => {
 const Skills = () => {
     return (
         <div className="skills-container">
-            Skills Container
+            <div className="skills-title">Skills</div>
+            <div className="skills-panels">
+                {SkillsData.map(function (item) {
+                    return <SkillPanel skillData={item}/>
+                })}
+            </div>
+            
         </div>
     );
 };
